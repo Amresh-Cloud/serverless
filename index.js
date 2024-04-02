5d80981c07d004f3560701747a42ab3511daf63b
AmreshRajvinderSingh <144646953+AmreshRajvinderSingh@users.noreply.github.com>
2024-04-01 23:48:35 -0400

    added domain in using ENV

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
-const Sequelize = require('sequelize');
-const sequelize = require('./connection');
 const User = require('./userSchema');
 const dotenv=require('dotenv');
 
@@ -17,18 +15,20 @@ const mailgunClient = mailgunFormData.client({
 exports.sendemail = async (event, context) => {
   console.log("trying");
   try {
-    const message = event.data
+    console.log("extracting message from pubsub");
+    const pubSubmessage = event.data
       ? Buffer.from(event.data, 'base64').toString()
       : '{}';
-    const payload = JSON.parse(message);
+    const payload = JSON.parse(pubSubmessage);
 
-    const userEmail = payload.username;
+    const userEmailAddress = payload.username;
     const userId=payload.id;
-    const verificationLink=`http://amreshdev.me:2500/v1/user/verify?id=${userId}`;
+    const verificationLink=`http://${process.env.DOMAIN_NAME}:2500/v1/user/verify?id=${userId}`;
+    console.log(verificationLink);
 
     const mailOptions = {
       from: '<mailgun@amreshdev.me>',
-      to: [userEmail],  
+      to: [userEmailAddress],  
       subject: 'Email Verification',
       text: 'Please verify your email address.',
       html: `<p>Verify your email address: <a href="${verificationLink}">link</a></p>`,
@@ -36,7 +36,7 @@ exports.sendemail = async (event, context) => {
 
 
     const response = await mailgunClient.messages.create(process.env.DOMAIN_NAME, mailOptions);
-    console.log('Email sent:', response);
+    console.log('Email Succefully sent:', response);
     if (response.id) {
  
       const user = await User.findOne({ where: { username: userEmail } });
