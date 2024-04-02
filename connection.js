ef73aa5bd35bf951526ff82dab0741dc31bd694f
AmreshRajvinderSingh <144646953+AmreshRajvinderSingh@users.noreply.github.com>
2024-04-01 23:48:35 -0400

    added domain in using ENV

--- connection.js
+++ connection.js
@@ -1,6 +1,5 @@
 const Sequelize=require('sequelize');
 const dotenv=require('dotenv');
-const mysql=require('mysql2/promise');
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASSWORD, {
